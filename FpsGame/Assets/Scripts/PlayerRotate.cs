aa813d5a77a90f898cf5774ee77301d8426ac4c5
kimhyeok <kimmang940715@naver.com>
2023-10-12 05:05:14 +0900

    플레이어 회전 구현 추가

--- FpsGame/Assets/Scripts/PlayerRotate.cs
+++ FpsGame/Assets/Scripts/PlayerRotate.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float rotSpeed = 200f;
+    private float mx = 0f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float mouse_X = Input.GetAxis("Mouse X");
+        mx += mouse_X * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, mx, 0);
     }
 }
