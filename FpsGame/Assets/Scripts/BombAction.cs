03e41fcbbc92f4ff76c7515081225d209ce93c41
kimhyeok <kimmang940715@naver.com>
2023-10-12 05:54:04 +0900

    폭발 위치 지정 및 Effect Instantiate

--- FpsGame/Assets/Scripts/BombAction.cs
+++ FpsGame/Assets/Scripts/BombAction.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 
 public class BombAction : MonoBehaviour
 {
+    public GameObject bombEffect;
     private void OnCollisionEnter(Collision other)
     {
+        GameObject eff = Instantiate(bombEffect);
+        
+        // 이펙트 프리팹의 위치는 수류탄 오브젝트의 위치
+        eff.transform.position = transform.position;
         Destroy(gameObject);
     }
 }
