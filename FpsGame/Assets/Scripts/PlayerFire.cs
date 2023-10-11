7f0ad7350955a8cff983953828f0ae21694310a4
kimhyeok <kimmang940715@naver.com>
2023-10-12 05:58:25 +0900

    수류탄 투척기능 추가

--- FpsGame/Assets/Scripts/PlayerFire.cs
+++ FpsGame/Assets/Scripts/PlayerFire.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 발사위치
+    public GameObject firePosition;
+    
+    // 투척 무기 오브젝트
+    public GameObject bombFactory;
+    
+    // 투척 파워
+    public float throwPower = 15f;
     void Start()
     {
         
@@ -13,6 +20,16 @@ public class PlayerFire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 마우스 우클릭을 누르면
+        if (Input.GetMouseButtonDown(1))
+        {
+            // 수류탄 오브젝트 생성
+            GameObject bomb = Instantiate(bombFactory);
+            // 생성위치를 발사위치로
+            bomb.transform.position = firePosition.transform.position;
+
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+            rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+        }
     }
 }
