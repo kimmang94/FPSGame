2b5bbbabb4b6e8d07429496e25f0a2189f8017f6
kimhyeok <kimmang940715@naver.com>
2023-10-12 05:09:30 +0900

    카메라가 바라보는방향이 앞이되게 이동구현 (하늘, 땅으로 움직이는것은 수정필요)

--- FpsGame/Assets/Scripts/PlayerController/PlayerMove.cs
+++ FpsGame/Assets/Scripts/PlayerController/PlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 7f;
     void Start()
     {
         
@@ -13,6 +13,19 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 1. 사용자 입력 받기
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
         
+        // 2. 이동 방향 설정
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = dir.normalized;
+        
+        // 2.1 메인 카메라를 기준으로 방향변환
+        dir = Camera.main.transform.TransformDirection(dir);
+        
+        
+        // 3. 이동 속도에 맞춰 이동
+        transform.position += dir * moveSpeed * Time.deltaTime;
     }
 }
