39c2045126695bd04ae93cfbc31960a5ad34331d
kimhyeok <kimmang940715@naver.com>
2023-10-12 05:18:09 +0900

    점프기능 구현

--- FpsGame/Assets/Scripts/PlayerController/PlayerMove.cs
+++ FpsGame/Assets/Scripts/PlayerController/PlayerMove.cs
@@ -8,6 +8,9 @@ public class PlayerMove : MonoBehaviour
     private CharacterController cc;
     private float gravity = -20f;
     private float yVelocity = 0; // y축 속도
+    public float jumpPower = 10f;
+    public bool isJumping = false;
+    
     void Start()
     {
         cc = GetComponent<CharacterController>();
@@ -26,6 +29,29 @@ public class PlayerMove : MonoBehaviour
         
         // 2.1 메인 카메라를 기준으로 방향변환
         dir = Camera.main.transform.TransformDirection(dir);
+        
+        // 만약 다시 바닥에 착지했다면
+        // CollisionFlag.Below 는 컨트롤러 하단에 충돌할경우 true
+        // Above 는 위
+        // Side 는 옆쪽 일경우 true
+        if (cc.collisionFlags == CollisionFlags.Below)
+        {
+            if (isJumping)
+            {
+                isJumping = false;
+                yVelocity = 0;
+            }
+            
+        // Jump 키를 누르면
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            // 캐릭터 수직속도에 점프력 더하기
+            yVelocity = jumpPower;
+            isJumping = true;
+        }
+
+       
+        }
         
         // 2.2 캐릭터 수직 속도에 중력 값을 적용
         yVelocity += gravity * Time.deltaTime;
